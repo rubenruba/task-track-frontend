fd7c3bbdb78af7d8688d540cf9bad4c86b8c728d
Ruben Home PC <rubenn.mmartinezz@gmail.com>
2024-04-21 21:14:09 +0200

    Add principal structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+
+export const App: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={''} />
+    </Routes>
+  )
+}
