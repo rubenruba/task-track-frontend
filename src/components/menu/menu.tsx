8f8882b978655b8da92420ef56d0d629c7e7e6fc
Ruben Home PC <rubenn.mmartinezz@gmail.com>
2024-05-09 01:07:04 +0200

    Add some responsive

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,5 +1,6 @@
 import LogoutIcon from '@mui/icons-material/Logout';
-import { FC } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import { FC, useRef } from "react";
 import { Link } from "react-router-dom";
 import { UserService } from "../../services/UserService";
 import './menu.sass';
@@ -7,26 +8,37 @@ import './menu.sass';
 export const MenuComponent: FC = () => {
     const userService = new UserService();
     const user = userService.getCurrentUser()?.user;
-    const darkBlue = { color: "#47B4EB", cursor: "pointer" };
+    const icon = { color: "#47B4EB", cursor: "pointer" };
+    const menuRef = useRef<HTMLDivElement | null>(null);
+
+    const openMenu = () => {
+        if (!menuRef.current) return;
+        if (menuRef.current.style.display === 'none') menuRef.current.style.display = 'flex';
+        else menuRef.current.style.display = 'none';
+    }
 
     return (
         <nav>
-            <div>
-                <img src={require('../../assets/logo.png')} alt="logo" /> 
-                <ul>
-                    <li><Link to='/calendar'>Calendar</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                    <li><Link to='/register'>Register</Link></li>
-                </ul>
-            </div>
+            <MenuIcon className='menu-icon' onClick={openMenu} sx={icon} />
+            <img src={require('../../assets/logo.png')} alt="logo" className='mobile-logo' /> 
+            <div className='menu-container' ref={menuRef}>
+                <div>
+                    <img src={require('../../assets/logo.png')} alt="logo" /> 
+                    <ul>
+                        <li><Link to='/calendar'>Calendar</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/register'>Register</Link></li>
+                    </ul>
+                </div>
 
-            <div className='nav-username'>
-                <p>{user?.username}</p>
-                <LogoutIcon 
-                    titleAccess='Logout' 
-                    onClick={userService.logout} 
-                    sx={darkBlue} 
-                />
+                <div className='nav-username'>
+                    <p>{user?.username}</p>
+                    <LogoutIcon 
+                        titleAccess='Logout' 
+                        onClick={userService.logout} 
+                        sx={icon} 
+                    />
+                </div>
             </div>
         </nav>  
     )
